0090d1699190f87346867424dbbaa26d149da173
Raghu Ram <raghuram.jayavarapu@gmail.com>
2024-12-24 06:52:31 +0530

    New password reset added

---
+++ reset_password.py
@@ -0,0 +1,7 @@
+username = input("Enter username: ")
+password = input("Eneter new password: ")
+if username == "Admin" and password == "Admin":
+    print("You can rest")
+else:
+    print("Cannot reset")
+    
